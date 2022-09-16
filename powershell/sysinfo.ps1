55a74d6e2977c969fd1bb514c560b1de46440e00
keyofmiracles <udehndukwe@gmail.com>
2022-09-15 21:48:41 -0400

    powershell files

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,3 +1,8 @@
-﻿function getIP{
-    (Get-NetIPAddress).Ipv4address | Select-string "192*"
-    }
+﻿$IP = (get-netipaddress).IPAddress[2]
+$hostname = hostname
+$version = $HOST.Version.Major
+$day = Get-Date -Format "dddd, MMMM dd yyy"
+
+$BODY = "This machine's IP is $IP." + " " + "User is $env:USERNAME." + " " + "Hostname is $hostname." + " " + "PowerShell Version $version." + " " + "Today's Date is $day."
+
+$BODY | Out-File C:\it3038c-scripts\Lab3output.txt
