664f5403ede9a69b10375da8c62ec3afa9da0683
keyofmiracles <udehndukwe@gmail.com>
2022-10-06 23:58:34 -0400

    Module 6

---
+++ powershell/copyallpdfs.ps1
@@ -0,0 +1,31 @@
+﻿mkdir $env:HOMEPATH\ALLPDFS
+
+$items = Get-ChildItem -Recurse -Filter "*.txt*" 
+
+$answer = Read-Host -Prompt "Make copies? Enter 'Yes' or 'No'"
+
+if ($answer -eq 'Yes') {
+
+$items.fullname | ForEach-Object {
+
+Copy-Item -Path $_ -Destination $env:HOMEPATH\ALLPDFS
+
+}
+
+    $ans2 = Read-Host -Prompt "Delete Old files? Enter 'Yes' or 'No'"
+    if ($ans2 -eq 'Yes') {
+
+        $items.FullName | ForEach-Object {
+            Remove-Item -Path $_ -Force -Recurse
+        }
+    }
+
+
+}
+
+if ($answer -eq 'No') {
+
+    Write "OK. Aborting."
+}
+
+
